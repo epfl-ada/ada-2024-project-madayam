e4f9fac215620cf87f68d11c7b29aa11fe26f10f
Amirmahdi <hosseinabadi26@gmail.com>
2024-11-15 23:52:38 +0100

    update methods

--- config/ada_config/config.py
+++ config/ada_config/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent.parent
 
 CONFIG = {
-    'data_path': ROOT_PATH / 'ADA Data',
-    'cmu_path': ROOT_PATH / 'ADA Data/CMU',
-    'tmdb_path': ROOT_PATH / 'ADA Data/TMDB',
+    'data_path': ROOT_PATH / 'ADAData',
+    'cmu_path': ROOT_PATH / 'ADAData/CMU',
+    'tmdb_path': ROOT_PATH / 'ADAData/TMDB',
 }
